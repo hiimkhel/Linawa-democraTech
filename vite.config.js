bffad048f1c790d5941e4ce23ebbad92d6b1bb20
ydrhan <kellyydrhan.alojepan@wvsu.edu.ph>
2025-04-28 11:15:45 +0800

    Frontend deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: './src',
+  build:{
+    outDir: '../dist'
+  },
   server: {
     port: process.env.PORT || 5173,  // Use the PORT environment variable, fallback to 5173 for local development
     strictPort: true,  // Ensure that the port is strictly followed (don't fall back to a different port)
