6c8145566b26193060068d137bb7b359df98526c
ydrhan <kellyydrhan.alojepan@wvsu.edu.ph>
2025-04-28 11:53:03 +0800

    Added domain to allowedhHosts

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   base: '/',
   plugins: [react()],
   server: {
-    allowedHosts: ['linawa-frontend.onrender.com'],
+    allowedHosts: ['linawa.onrender.com'],
     host:'0.0.0.0',
     port: process.env.PORT || 5173,  // Use the PORT environment variable, fallback to 5173 for local development
     strictPort: true,  // Ensure that the port is strictly followed (don't fall back to a different port)
