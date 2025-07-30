902d45075fe48ece4e5df2402ebeb7d03318622d
ydrhan <kellyydrhan.alojepan@wvsu.edu.ph>
2025-07-30 21:09:08 +0800

    Redeployment #3

--- src/firebase.js
+++ src/firebase.js
@@ -6,6 +6,7 @@ import {getAuth} from "firebase/auth";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+console.log(import.meta.env.VITE_FIREBASE_API_KEY);
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
